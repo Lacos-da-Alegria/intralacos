cd34f5509b6cf95d3b78a0f10330bf8cdba2629b
Vitor Nere <vitor.nere@hotmail.com>
2017-10-03 18:12:59 -0300

    Adicionado img do background do login

--- src/app/components/autenticacao/autenticacao.component.ts
+++ src/app/components/autenticacao/autenticacao.component.ts
@@ -5,11 +5,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './autenticacao.component.html',
   styleUrls: ['./autenticacao.component.scss']
 })
-export class AutenticacaoComponent implements OnInit {
+export class AutenticacaoComponent {
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor() {
+    document.body.style.background = 'url(\'../../../assets/img/login-bg.jpg\') no-repeat center center fixed';
+    document.body.style.height = '100%';
+    document.body.style.backgroundSize = 'cover';
+  }
+
+  OnDestroy() {
+    document.body.style.background = 'none';
+    document.body.style.height = 'none';
+    document.body.style.backgroundSize = 'none';
   }
 
 }
